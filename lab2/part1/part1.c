139741e536e03fb11aa8645a91610a5cf2e0bd3b
fusion2004 <fusion2004@gmail.com>
2011-09-22 14:09:45 -0400

    added the mutex var, wrote comments for main()

--- lab2/part1/part1.c
+++ lab2/part1/part1.c
@@ -11,44 +11,66 @@
 
 #include "part1.h"
 
+// The magical shared variable all the threads want to modify.
 int shared_variable = 0;
+
+// Some beautiful pthread constructs to stop the threads from BREAKING
+// the wonderful shared variable.
+pthread_mutex_t   shavar_mutex;
 pthread_barrier_t barr;
 
 int main(int argc, char *argv[]) {
+  // If there are more or less than 2 arguments, we assume the user has typed
+  // something incorrectly, and print out the Usage.
   if(argc != 2) {
     printUsage(argv[0]);
-  } else {
-    int num_threads = atoi(argv[1]);
-    char check_it[strlen(argv[1]) + 1];
+  }
 
+  // If we have 2 args, parse the 2nd arg into an integer to define how
+  // many threads to spawn. And then... we spawn 'em.
+  else {
+    int num_threads = atoi(argv[1]);
+
+    // Now we parse the int BACK into a char* so we can double-check it
+    // with the original arg. There may be an easier way of doing this.
+    char check_it[strlen(argv[1]) + 1];
     sprintf(check_it, "%d", num_threads);
 
+    // If it doesn't match, we'll print out an error and exit the program.
     if(strcmp(argv[1], check_it) != 0) {
       printInputError();
-    } else {
+      return -1;
+    }
+
+    // If it does match, we'll continue and start spawning threads.
+    else {
       int i;
       pthread_t th_id[num_threads+1];
 
 #ifdef PTHREAD_SYNC
+      // We'll initialize the barrier used to sync all the threads before
+      // printing out the final value of the shared variable.
       if(pthread_barrier_init(&barr, NULL, num_threads)) {
         printf("Error: Could not create a barrier\n");
         return -1;
       }
 #endif
 
+      // Spawn the threads.
       for(i = 1; i <= num_threads; i++) {
         if(pthread_create(&th_id[i], NULL, (void *)&SimpleThread, (void *)i)) {
           printf("Error: Could not create thread %d\n", i);
           return -1;
         }
       }
+
+      // Join the main thread to all of the threads we just spawned.
       for(i = 1; i <= num_threads; i++) {
         if(pthread_join(th_id[i], NULL)) {
           printf("Error: Could not join thread %d\n", i);
           return -1;
         }
       }
-
     }
   }
   return 0;
@@ -65,7 +87,6 @@ void printInputError() {
 
 void SimpleThread(void *args) {
   int which = (int)args;
-  //printf("Thread started with value: %d\n", which);
   int num, val;
 
   for(num = 0; num < 20; num++) {
