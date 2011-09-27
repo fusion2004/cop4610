029c40d880d4ec04e1fa3231e2c6cca7cf679efa
fusion2004 <fusion2004@gmail.com>
2011-09-27 09:44:11 -0400

    main() does some input error checking, part2

--- lab2/part2/part2.c
+++ lab2/part2/part2.c
@@ -11,6 +11,7 @@
 #include "part2.h"
 
 int question_asker = -1;
+int num_conference;
 
 pthread_mutex_t speech_mutex;
 pthread_mutex_t question_mutex;
@@ -18,10 +19,29 @@ pthread_mutex_t speaker_mutex;
 pthread_cond_t  speaker_cond;
 
 int main(int argc, char *argv[]) {
+  if(argc != 3) {
+    printf("usage: usage goes here\n");
+  }
+  else {
+    int num_threads = atoi(argv[1]);
+    num_conference  = atoi(argv[2]);
 
-  pthread_t speaker;
-  pthread_t reporters[];
+    char check_num_threads[strlen(argv[1] + 1);
+    char check_num_conference[strlen(argv[2] + 1);
 
+    sprintf(check_num_threads, "%d", num_threads);
+    sprintf(check_num_conference, "%d", num_conference);
+
+    if(strcmp(argv[1], check_num_threads) != 0 ||
+       strcmp(argv[2], check_num_conference != 0) {
+      printf("some errors\n");
+      return -1;
+    }
+
+    pthread_t speaker;
+    pthread_t reporters[num_threads];
+
+  }
   return 0;
 }
 
