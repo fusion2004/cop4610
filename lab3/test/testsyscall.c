e30c13366821499925d2985a434b499fc88afeae
Alexis <alexis@alexis-laptop.(none)>
2011-11-03 11:04:44 -0400

    I added the new amt_free system call to the testmycall.c program and testmycall.h

--- lab3/test/testsyscall.c
+++ lab3/test/testsyscall.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #include "testsyscall.h"
 
@@ -7,19 +6,20 @@ int main (void)
 {
 	char * buffer0, buffer1, buffer2, buffer3, buffer4, buffer5, buffer6, buffer7;
 
-        printf("%d\n", syscall(__NR_get_slob_amt_claimed));
+     //   printf("%d\n", syscall(__NR_get_slob_amt_claimed));
 
         buffer0 = (char*) malloc (9000000);
         buffer1 = (char*) malloc (345555);
-        printf("%d\n", syscall(__NR_get_slob_amt_claimed));
+  //      printf("%d\n", syscall(__NR_get_slob_amt_claimed));
         buffer2 = (char*) malloc (1234);
         buffer3 = (char*) malloc (144445554);
         buffer4 = (char*) malloc (55630);
-        printf("%d\n", syscall(__NR_get_slob_amt_claimed));
+//        printf("%d\n", syscall(__NR_get_slob_amt_claimed));
         buffer5 = (char*) malloc (10064666665);
-        buffer6 = (char*) malloc (105555555550);
-        buffer7 = (char*) malloc (1000000000);
+  //      buffer6 = (char*) malloc (105555555550);
+    //    buffer7 = (char*) malloc (1000000000);
 
+	printf("%d\n", syscall(__NR_get_slob_amt_free));
         printf("%d\n", syscall(__NR_get_slob_amt_claimed));
 	
         return 0;
--- lab3/test/testsyscall.h
+++ lab3/test/testsyscall.h
@@ -1,3 +1,4 @@
 #include <linux/unistd.h>
 
 #define __NR_get_slob_amt_claimed 343
+#define __NR_get_slob_amt_free 344
