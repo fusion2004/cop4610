de49b4fb54f3476841eafab45cd279a50219b725
Alexis <alexis@alexis-laptop.(none)>
2011-11-06 11:23:08 -0500

    I fixed the testsyscall.c file to be a blittle bit nicer looking but I'm not sure how to get rid of all those warnings without doing a lot of extra work.

--- lab3/test/testsyscall.c
+++ lab3/test/testsyscall.c
@@ -6,21 +6,17 @@ int main (void)
 {
 	char * buffer0, buffer1, buffer2, buffer3, buffer4, buffer5, buffer6, buffer7;
 
-     //   printf("%d\n", syscall(__NR_get_slob_amt_claimed));
-
         buffer0 = (char*) malloc (9000000);
         buffer1 = (char*) malloc (345555);
-  //      printf("%d\n", syscall(__NR_get_slob_amt_claimed));
         buffer2 = (char*) malloc (1234);
         buffer3 = (char*) malloc (144445554);
         buffer4 = (char*) malloc (55630);
-//        printf("%d\n", syscall(__NR_get_slob_amt_claimed));
         buffer5 = (char*) malloc (10064666665);
-  //      buffer6 = (char*) malloc (105555555550);
-    //    buffer7 = (char*) malloc (1000000000);
+        buffer6 = (char*) malloc (105555555550);
+        buffer7 = (char*) malloc (1000000000);
 
-	printf("Amount Free: %d\n", syscall(__NR_get_slob_amt_free));
-        printf("Amount Claimed: %d\n", syscall(__NR_get_slob_amt_claimed));
-	
+	printf("Amount Free: %d bytes\n", syscall(__NR_get_slob_amt_free));
+        printf("Amount Claimed: %d bytes\n", syscall(__NR_get_slob_amt_claimed));
+
         return 0;
 }
