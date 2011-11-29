e2a7839d25f223b22b9c212afa72bdb758322afa
fusion2004 <fusion2004@gmail.com>
2011-11-29 11:01:04 -0500

    added printk for adding requests

--- lab4/clook-iosched.c
+++ lab4/clook-iosched.c
@@ -43,6 +43,9 @@ static void clook_add_request(struct request_queue *q, struct request *rq)
 		}
 	}
 	list_add_tail(&rq->queuelist, cur);
+
+	char readwrite = (&rq->cmd_flags & REQ_WRITE) ? 'W' : 'R';
+	printk("[CLOOK] add %c %u", readwrite, rq_end_sector(rq));
 }
 
 static int clook_queue_empty(struct request_queue *q)
